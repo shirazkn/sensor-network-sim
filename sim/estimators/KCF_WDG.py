1d96b14775d0cf2b28e2d537a76c8f12f87cc289
shirazkn <shiraz.yellow@gmail.com>
2019-11-30 08:28:22 -0500

    Rename and update files

--- sim/estimators/KCF_WDG.py
+++ sim/estimators/KCF_WDG.py
@@ -25,19 +25,21 @@ class EstimatorKCF_WC(sim.sensor.Sensor):
     def __init__(self, all_sensor_ids, adj_matrix, **kwargs):
 
         super().__init__(**kwargs)
+        self.all_sensors = all_sensor_ids  # Global information is required
+
         self.estimate_prior = column(np.array([20, 0]))
+        self.ErrCov_prior = 50*np.identity(2)  # For all i, i
+        self.CrossCov_prior = np.zeros([2, 2])  # For all i, j pairs
 
-        self.ErrCov_prior = 50*np.identity(2)
-        CrossCov_prior = 10*np.identity(2)
-
-        # Dict of P_ij (Cross-covariance) Matrices, where i = self.id
+        # Prior and posterior Cross-covariance matrices, where i = self.id
         self.P = {i_ind: {} for i_ind in all_sensor_ids}
-        for i_ind, row in self.P.items():
-            for j_ind in all_sensor_ids:
-                row[j_ind] = CrossCov_prior
-            row[i_ind] = self.ErrCov_prior
-
         self.M = deepcopy(self.P)
+
+    def initialize(self):
+        for i_ind, row in self.P.items():
+            for j_ind in self.all_sensors:
+                row[j_ind] = self.CrossCov_prior
+            row[i_ind] = self.ErrCov_prior
 
     def do_estimation(self, target_info: dict, all_sensors: Dict[str, sim.sensor.Sensor]):
         """
@@ -46,9 +48,12 @@ class EstimatorKCF_WC(sim.sensor.Sensor):
         """
 
         _i = self.id
+        # Consensus gains Cji are stored as C{i: {j: ..., ...}, ...} where j are neighbors of i
         C_gains = {_id: get_C_gains(_id, all_sensors) for _id in all_sensors.keys()}
+        # Kalman gains Ki are stored as K{i: ...}
         K_gains = {_id: get_K_gain(_id, all_sensors, C_gains) for _id in all_sensors.keys()}
 
+        # F = I - KH
         F = {_id:
              (np.identity(2) - K_gains[_id] @ all_sensors[_id].Obs)
              for _id in all_sensors.keys()}
@@ -78,18 +83,25 @@ class EstimatorKCF_WC(sim.sensor.Sensor):
                                 + target_info["B"] @ target_info["NoiseCov"] @ target_info["B"].T)
 
         self.ErrCov = self.M[_i][_i]
-        self.estimate = self.estimate_prior + K_gains[self.id] @ (self.measurement - self.Obs @ self.estimate_prior) \
+        self.estimate = self.estimate_prior + K_gains[self.id] @ (self.measurement - self.Obs @ self.estimate_prior)\
                         + sum([C_gains[_i][j] @ (all_sensors[j].estimate_prior - self.estimate_prior)
                                for j in self.neighbors])
 
         self.estimate_prior = target_info["A"] @ self.estimate
 
 
-def get_C_gains(ID, sensors):
-    _i = ID
-    sensor = sensors[ID]
+def get_C_gains(_id, sensors):
+    """
+    Compute consensus gains for a sensor
+    :param _id: ID of sensor with index i ( as in C_ji )
+    :param sensors: All sensors
+    :return: dict: {"neighbor_ID": [numpy.array]}
+    """
+    _i = _id
+    sensor = sensors[_id]
     _C_gains = {_id: np.zeros([2, 2]) for _id in sensor.neighbors}
 
+    # Solve matrix equality (using local information)
     A = [[np.zeros([2, 2]) for _ in sensor.neighbors] for _ in sensor.neighbors]
     B = [np.zeros([2, 2]) for _ in sensor.neighbors]
     Del_inv = la.pinv(sensor.NoiseCov + sensor.Obs @ sensor.P[_i][_i] @ sensor.Obs.T)
@@ -106,13 +118,17 @@ def get_C_gains(ID, sensors):
     A_inv = la.pinv(np.block(A))
     C = (np.block(B) @ A_inv)
 
+    # Separate C into 2x2 matrices
     for ind, _j in enumerate(sensor.neighbors):
-        _C_gains[_j] = C[0:2, ind:ind+2]
+        _C_gains[_j] = C[0:2, ind*2:(ind+1)*2]
 
     return _C_gains
 
 
 def get_K_gain(ID, sensors, C_gains):
+    """
+    Compute Kalman gain for a sensor
+    """
     _i = ID
     sensor = sensors[ID]
     sum_term = sum([C_gains[_i][_r] @ (sensor.P[_r][_i] - sensor.P[_i][_i]) for _r in sensor.neighbors])
